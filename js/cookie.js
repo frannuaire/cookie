ad11b020840860e9ccf8860105573978fa5c07c8
Yohann Boniface <edhyjox@gmail.com>
2021-07-07 17:00:09 +0200

    - extracting css to constant, improving code readability - basic refactoring (rename/space) - setting english link for english language

--- js/cookie.js
+++ js/cookie.js
@@ -21,38 +21,78 @@
  @version 2.0
  **/
 
+const legacyCss = css = `
+    .cookie {
+        position: fixed;          
+        left: 0;
+        right: 0;
+        bottom: 0;
+        padding: 10px;
+        min-height: 90px;
+        line-height: 20px;
+        text-align:center;
+        color: white; 
+        opacity: .7;
+        background-color : #000a;
+        box-shadow: 0 0 12px #000000;
+        z-index: 99999;
+    }
+    
+    p {
+        width: 100%;
+    }
+    
+    #btnHide {
+        color:#e97200;
+        text-align:right;
+        text-decoration:none;
+        padding-left:10px;
+    }
+    
+    .hideCookie {
+        display:none;
+    }
+                    
+    .cookie a {
+        color:#f08c00;
+    }
+`;
+
 /**
  * Objet permettant de gérer le message d' acceptation des cookies.
  */
 class Cookie {
 
-    constructor(language = "fr", aCss = "") {
+    constructor(language = "fr", customCSS = undefined) {
         this.langue = language;
 
         this.setMessage();
         this.bInit = false;
-        this.init(aCss);
+        this.init(customCSS);
     }
 
     setMessage() {
         switch (this.langue) {
             case "fr":
                 this.message = `
-                    En continuant à naviguer sur ce site, vous acceptez l'utilisation de cookies pour disposer de services adaptés à vos centres d' intérêts.
+                    En continuant à naviguer sur ce site, vous acceptez l'utilisation de cookies pour disposer de 
+                    services adaptés à vos centres d' intérêts.
                     <a href='https://www.google.com/intl/fr/policies/technologies/cookies/'>En savoir plus</a>
                 `;
                 break;
             case "en":
                 this.message = `
-                    By continuing to browse this site, you accept the use of cookies to provide services tailored to your interests.
-                    <a href='https://www.google.com/intl/fr/policies/technologies/cookies/'>Know more</a>
+                    By continuing to browse this site, you accept the use of cookies to provide services tailored to 
+                    your interests.
+                    <a href='https://www.google.com/intl/en/policies/technologies/cookies/'>Know more</a>
                     `;
                 break;
 
             default:
                 this.message = `
-                    By continuing to browse this site, you accept the use of cookies to provide services tailored to your interests.
-                    <a href='https://www.google.com/intl/fr/policies/technologies/cookies/'>Know more</a>
+                    By continuing to browse this site, you accept the use of cookies to provide services tailored to
+                     your interests.
+                    <a href='https://www.google.com/intl/en/policies/technologies/cookies/'>Know more</a>
                 `;
         }
     }
@@ -92,54 +132,12 @@ class Cookie {
     /**
      *  Création des éléments HTML et css que l' on ajoute ensuite au body
      **/
-    createElt(aCss) {
-
-        if (aCss === '') {
-            this.css = `
-                .cookie {
-                    position: fixed;
-                             
-                    left: 0;
-                    right: 0;
-                    bottom: 0;
-                    
-                    padding: 10px;
-                    min-height: 90px;
-                    line-height: 20px;
-                    text-align:center;
-
-                    color: white; 
-                    opacity: .7;
-                    background-color : #000a;
-                    box-shadow: 0 0 12px #000000;
-                    z-index: 99999;
-                }
-                
-                p {
-                  width: 100%;
-                }
-                
-                #btnHide{
-                    color:#e97200;
-                    text-align:right;
-                    text-decoration:none;
-                    padding-left:10px;
-                }
-                    
-                .hideCookie{
-                    display:none;
-                }
-                
-                .cookie a{
-                    color:#f08c00;
-                }`;
-
-        } else {
-            this.css = aCss;
-        }
+    createElt(customCss) {
+        this.css = (customCss === undefined) ? legacyCss: customCss;
 
         let head = document.head || document.getElementsByTagName("head")[0];
         let dvCookie = document.createElement("div");
+
         let style = document.createElement("style");
         style.type = "text/css";
         if (style.styleSheet) {
@@ -148,17 +146,21 @@ class Cookie {
             style.appendChild(document.createTextNode(this.css));
         }
         head.appendChild(style);
+
         dvCookie.setAttribute("id", "cookie");
         dvCookie.setAttribute("class", "cookie");
+
         let pMsg = document.createElement("p");
         pMsg.id = "pMsg";
         pMsg.innerHTML = this.message;
-        //  dvCookie.innerHTML = this.message ;
+
+        // dvCookie.innerHTML = this.message ;
         let fermer = document.createElement("a");
         fermer.setAttribute("id", "btnHide");
         fermer.setAttribute("class", "btnHide");
         fermer.setAttribute("href", "#");
         fermer.innerHTML = "&#x274C;";
+
         dvCookie.appendChild(pMsg);
         dvCookie.appendChild(fermer);
         document.body.appendChild(dvCookie);
@@ -177,7 +179,7 @@ class Cookie {
 
     /* To set legal Text */
     setText(text) {
-        let dvCookie = document.getElementById("spanMsg");
+        let dvCookie = document.getElementById("pMsg");
         dvCookie.innerHTML = text;
     }
 }
